adb3b49ba7dcbc8af228440bbb6faff187371a0d
KrylovAnton <krylovanton@list.ru>
2021-12-12 17:04:33 +1100

    Create LessonOne.java
    Дз урок 1

---
+++ LessonOne.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.homework;
+
+public class LessonOne {
+        public static void main(String[] args) {
+            printThreeWords();
+            checkSumSign();
+            printColor();
+            compareNumbers();
+        }
+
+        static void printThreeWords() {
+            String multipleLines = "Orang"
+                    + System.lineSeparator() + "Banana"
+                    + System.lineSeparator() + "Apple";
+            System.out.println(multipleLines);
+        }
+
+        static void checkSumSign() {
+            int a = 17;
+            int b = 21;
+            System.out.println((a + b >= 0)? "sum positive" : "sum negative");
+        }
+
+        static void printColor() {
+            int value = 35;
+            if (value <= 0) {
+                System.out.println("red");
+            }
+            else if (value <= 100) {
+                System.out.println("yellow");
+            }
+            if (value >100) {
+                System.out.println("green");
+            }
+        }
+
+        static void compareNumbers() {
+            int a = 50;
+            int b = 45;
+            System.out.println((a >= b)? "a >= b" : "a < b");
+        }
+    }
